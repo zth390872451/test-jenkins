ed1aaac61f59775758077c0061383624c698be9e
zth390872451 <zth390872451@gmail.com>
2016-12-05 18:21:31 +0800

    启动日志信息

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -1,15 +1,34 @@
 package test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Created by Administrator on 2016/11/23.
  */
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) throws UnknownHostException {
         System.out.println("Application.class args =  "+args);
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+
+        Environment env = app.run(args).getEnvironment();
+        log.info("Access URLs:\n ----------------------------------------------------------\n\t" +
+                        "Local: \t\t http://127.0.0.1:{}\n\t" +
+                        "External: \t http://{}:{} \n----------------------------------------------------------",
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
     }
 }
