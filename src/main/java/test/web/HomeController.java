2ae9570d8d14644c3f43f1da0c5aca020fe3aa7e
zth390872451 <zth390872451@gmail.com>
2016-12-07 18:03:23 +0800

    test

--- src/main/java/test/web/HomeController.java
+++ src/main/java/test/web/HomeController.java
@@ -1,15 +1,20 @@
 package test.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {  
   
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {  
         return "world";  
-    }  
+    }
+    @ResponseBody
+    @RequestMapping("/")
+    public String test() {
+        return "test";
+    }
 }  
