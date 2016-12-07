a4c9fa2dda8c98482973c695c937355955998860
Administrator <zth390872451@gmail.com>
2016-12-07 21:11:57 +0800

    日志文件

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -17,18 +17,23 @@ import java.net.UnknownHostException;
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
         System.out.println("Application.class args =  "+args);
         SpringApplication app = new SpringApplication(Application.class);
         SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
 
         Environment env = app.run(args).getEnvironment();
-        log.info("Access URLs:\n ----------------------------------------------------------\n\t" +
-                        "Local: \t\t http://127.0.0.1:{}\n\t" +
-                        "External: \t http://{}:{} \n----------------------------------------------------------",
-                env.getProperty("server.port"),
-                InetAddress.getLocalHost().getHostAddress(),
-                env.getProperty("server.port"));
+        try {
+            log.info("Access URLs:\n ----------------------------------------------------------\n\t" +
+                            "Local: \t\t http://127.0.0.1:{}\n\t" +
+                            "External: \t http://{}:{} \n----------------------------------------------------------",
+                    env.getProperty("server.port"),
+                    InetAddress.getLocalHost().getHostAddress(),
+                    env.getProperty("server.port"));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        log.error("test success???");
         System.out.println("true = " + true);
     }
 }
